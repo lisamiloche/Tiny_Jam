d228147a43868876b74acdafcd0fb9606930deda
lisamiloche <124432385+lisamiloche@users.noreply.github.com>
2024-10-04 10:01:02 +0200

    Level 01 qui fonctionne jusqu'au bout, debut Lvl 02 qui va fonctionner et 2/3 trucs

--- Tiny_Jam/Assets/Script/EnemyScript.cs
+++ Tiny_Jam/Assets/Script/EnemyScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
@@ -20,9 +21,19 @@ public class EnemyScript : MonoBehaviour
     {
         Collider2D[] _collidersPlayer = new Collider2D[2];
 
-        bool currentPlayer =
-            Physics2D.OverlapCircleNonAlloc(new Vector2 (transform.position.x + 0.15f, transform.position.y - 0.25f), _colRadius, _collidersPlayer, _defaultMask) > 0;
-        _isCol = currentPlayer;
+        if(transform.localScale == new Vector3 (1, 1, 1))
+        {
+            bool currentPlayer =
+            Physics2D.OverlapCircleNonAlloc(new Vector2(transform.position.x + 0.15f, transform.position.y - 0.25f), _colRadius, _collidersPlayer, _defaultMask) > 0;
+            _isCol = currentPlayer;
+        }
+        else
+        {
+            bool currentPlayer =
+            Physics2D.OverlapCircleNonAlloc(new Vector2(transform.position.x - 0.15f, transform.position.y - 0.25f), _colRadius, _collidersPlayer, _defaultMask) > 0;
+            _isCol = currentPlayer;
+        }
+        
     }
 
     private void OnDrawGizmos()
